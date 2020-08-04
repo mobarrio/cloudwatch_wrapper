bf41c4709a2408a4791b768a2b5f1b4aa1a5a661
Mariano J. Obarrio Miles <78178@globalia.com>
2020-08-04 20:13:58 +0200

    Update de metodos en la documentacion de la API

--- routes/authentication.js
+++ routes/authentication.js
@@ -30,6 +30,7 @@ function VerifyHandler(req, res, next) {
 function TTLHandler(req, res, next) {
  try {
    const credentials = auth(req);
+   console.log(req.body.Config);
    const ttl = '1h';
    var account = ((typeof req.params.account === 'undefined') ? ((typeof req.body.Config === 'undefined' || typeof req.body.Config.Account === 'undefined' || req.body.Config.Account === "") ? 'pro' : req.body.Config.Account) : req.params.account);
    if(credentials != undefined && (credentials.name === "admin" && credentials.pass === "zabbix")) {
@@ -76,8 +77,7 @@ function LoginHandler(req, res, next) {
 router.get('/', LoginHandler);
 router.post('/verify', VerifyHandler);
 router.post('/get/credentials', TTLHandler);
-router.post('/get/credentials/:account', TTLHandler);
-router.post('/get/credentials/:account/ttl', TTLForeverHandler);
-router.post('/get/credentials/:account/ttl/:ttl', TTLForeverHandler);
+router.post('/get/credentials/ttl', TTLForeverHandler);
+router.post('/get/credentials/ttl/:ttl', TTLForeverHandler);
 
 module.exports = router;
